d274a348704539bc0cac0e1d3f3e0c04a76a1039
RyanLw007 <kap21tzu@uea.ac.uk>
2024-03-06 20:34:04 +0000

    adding some extra lines
    there were some lines missing which meant that kubernetes was still not installed properly

--- scripts/ck_slave.sh
+++ scripts/ck_slave.sh
@@ -21,6 +21,7 @@ export KUBECONFIG=$KUBEHOME/admin.conf
 sudo chsh -s /bin/bash $username
 echo "export KUBECONFIG=${KUBECONFIG}" > $HOME/.profile
 
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 sudo mkdir -p -m 755 /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
@@ -56,6 +57,7 @@ ctr image pull docker.io/andrewferguson/corekube-worker5g:latest
 
 # learn from this: https://blog.csdn.net/yan234280533/article/details/75136630
 # more info should see: https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/
+sudo apt-get update
 sudo apt-get -y kubelet=1.28.1-1.1 kubeadm=1.28.1-1.1 kubectl=1.28.1-1.1 kubernetes-cni golang-go jq
 sudo docker version
 sudo swapoff -a
