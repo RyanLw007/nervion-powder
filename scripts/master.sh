91829259a44695e8e1fe6575cb28d760f6e0291c
RyanLw007 <kap21tzu@uea.ac.uk>
2024-03-09 21:19:12 +0000

    forgot to add one line
    forgot to add the line to download runc

--- scripts/master.sh
+++ scripts/master.sh
@@ -49,6 +49,7 @@ python-pip automake autoconf libtool indent vim tmux ctags xgrep
 echo "containerd is installed below --------------------------------------------------------------------------------------------------"
 # docker
 
+wget https://github.com/opencontainers/runc/releases/download/v1.1.3/runc.amd64
 sudo install -m 755 runc.amd64 /usr/local/sbin/runc
 wget https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
 sudo mkdir -p /opt/cni/bin
@@ -56,7 +57,7 @@ sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.1.1.tgz
 
 wget https://github.com/containerd/containerd/releases/download/v1.6.8/containerd-1.6.8-linux-amd64.tar.gz
 tar Cxzvf /usr/local containerd-1.6.8-linux-amd64.tar.gz
-wget https://github.com/opencontainers/runc/releases/download/v1.1.3/runc.amd64
+
 
 sudo systemctl daemon-reload
 sudo systemctl enable --now containerd
